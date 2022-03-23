fb328e61ea09ebf982a2639b6d08863d4177d1a5
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-03-23 11:36:28 -0300

    ADD - date.cpp

---
+++ date.cpp
@@ -0,0 +1,34 @@
+#include "date.h"
+#include <string>
+
+
+using namespace std;
+
+string abbreviate(Date date){
+    switch(date.month){
+        case 1:
+            return "JAN";
+        case 2:
+            return "FEB";
+        case 3:
+            return "MAR";
+        case 4:
+            return "APR";
+        case 5:
+            return "MAY";
+        case 6:
+            return "JUN";
+        case 7:
+            return "JUL";
+        case 8:
+            return "AUG";
+        case 9:
+            return "SEP";
+        case 10:
+            return "OCT";
+        case 11:
+            return "NOV";
+        default:
+            return "DEC";
+    }
+}
