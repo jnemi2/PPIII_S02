c1e2717567860ae2511ffbcf01dc08b1c39f054b
Juan Bautista Nemi <80529710+jnemi2@users.noreply.github.com>
2022-03-29 18:26:50 -0300

    ADD - Assignment 1.1

--- main.cpp
+++ main.cpp
@@ -3,11 +3,22 @@
 
 using namespace std;
 
+float calculate_fee(float price, int hours, int minutes);
+
 int main()
 {
-    Date date;
-    date.month = 3;
-    cout<<abbreviate(date);
+    int hours, minutes;
+    printf("Hours:");
+    cin>>hours;
+    printf("Minutes:");
+    cin>>minutes;
+    printf("You owe $%.2f", calculate_fee(2.0, hours, minutes));
 
     return 0;
+}
+
+float calculate_fee(float price, int hours, int minutes){
+    float fee = price * hours;
+    if (minutes >= 15) fee += price;
+    return fee;
 }
